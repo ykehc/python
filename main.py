ee671ccd225f53c6e9afa686a7ebe8ee0c835479
Cheky <pantaleoncheky@gmail.com>
2025-04-09 17:36:21 -0400

    Tareas y Nuevos Detalles

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 # Esto es un comentario
 
-print("Hola mundo")
+print("Hola mundo\n")
 
 # Tarea 1: imprime tu nombre junto a tu edad
 
-print("Mi nombre es Cheky y tengo 19 años")
+print("Mi nombre es Cheky y tengo 19 años\n")
+
+# Tema 2: Operaciones
+
+print(f"{2 < 5} \n")
+
+# Tema 2.1: Variables
+
+nombre, edad = "Alexis", 25
+print(f"Hola soy {nombre} y tengo {edad} anio \n")
+
+# Tarea 2: Crea variables que contengan la informacion de nombre, ciudad y genero y imprima por cadena interpolada. (f"{insertar}")
+
+nombre, ciudad, genero = "Cheky", "Las Guaranas", "macho"
+print(f"Hola soy {nombre}, soy de {ciudad}, y soy un {genero} \n")
